f3cfdeaa3fd709095ad482fe721f6b6bda105474
peepeds <edbertmac@gmail.com>
2024-10-05 03:59:50 +0700

    fix: algo

--- index.js
+++ index.js
@@ -7,36 +7,50 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
-    const filePath = path.join(__dirname, 'views', 'index.html'); 
-    res.sendFile(filePath ); // Mengirim file ke client
+    const filePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(filePath); // Mengirim file ke client
 });
+
+
+const isValidDate = (date) => {
+	return !isNaN(date.getTime());
+}
 
 app.get('/api', (req, res) => {
-	const date = new Date();
-	const unix = date.getTime();
-	const utc = date.toUTCString();
-	res.json({unix,utc});
+    const date = new Date();
+    res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString(),
+    });
 });
 
-app.get('/api/:date', async (req, res) => {
-    const date = await req.params.date;
-	const regex = /^\d{4}-\d{2}-\d{2}$/;
-	if (regex.test(date)) {
-		const utc = new Date(date).toUTCString();
-		if (utc === 'Invalid Date') {
-			res.json({error: 'Invalid Date'});
-		}
-		const unix = new Date(date).getTime();
-		res.json({unix,utc});
-	} 
-	const unix = Number(date);
-	const utc = new Date(unix).toUTCString();
-	res.json({unix,utc});
-})
+app.get('/api/:date', (req, res) => {
+    const dateString = req.params.date;
+
+    if (!isNaN(dateString)) {
+        const unix = parseInt(dateString); // Convert to number
+        const date = new Date(unix);
+        if (isValidDate(date)) {
+            return res.json({
+                unix: unix,
+                utc: date.toUTCString(),
+            });
+        }
+    }
+
+    const date = new Date(dateString);
+    if (isValidDate(date)) {
+        return res.json({
+            unix: date.getTime(),
+            utc: date.toUTCString(),
+        });
+    } else {
+        return res.json({ error: "Invalid Date" });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
